4bed7043e204aa0bb0adff9153cbf6631a320e62
mohsensami <mohsensami18727@gmail.com>
2025-08-24 18:14:16 +0330

    sample products and product list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,17 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 const HomePage = () => {
-  return <h1>HomePage</h1>;
+  return (
+    <div className="space-y-8">
+      <h2 className="h2-bold">Latest Products</h2>
+      <ProductList
+        title="Newest Arrivals"
+        data={sampleData.products}
+        limit={4}
+      />
+    </div>
+  );
 };
 
 export default HomePage;
