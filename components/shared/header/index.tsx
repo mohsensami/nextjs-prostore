ac12bda6a9fd39a01f98c4820913f876486a706d
mohsensami <mohsensami18727@gmail.com>
2025-08-24 17:28:35 +0330

    responsive sheet menu

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,44 +1,28 @@
-import { ShoppingCart, UserIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-
-import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
-import ModeToggle from "./mode-toggle";
+import Menu from "./menu";
 
 const Header = () => {
   return (
     <header className="w-full border-b">
       <div className="wrapper flex-between">
         <div className="flex-start">
-          <Link href="/" className="flex-start">
+          <Link href="/" className="flex-start ml-4">
             <Image
-              priority={true}
               src="/images/logo.svg"
-              width={48}
-              height={48}
               alt={`${APP_NAME} logo`}
+              height={48}
+              width={48}
+              priority={true}
             />
             <span className="hidden lg:block font-bold text-2xl ml-3">
               {APP_NAME}
             </span>
           </Link>
         </div>
-        <div className="space-x-2">
-          <ModeToggle />
-          <Button asChild variant="ghost">
-            <Link href="/cart">
-              <ShoppingCart />
-              Cart
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href="/sign-in">
-              <UserIcon />
-              Sign In
-            </Link>
-          </Button>
-        </div>
+        <div className="hidden md:block"></div>
+        <Menu />
       </div>
     </header>
   );
