21b5237792a1dfb5dedcae5f0e99b6471cc2d95e
mohsensami <mohsensami18727@gmail.com>
2025-08-25 21:44:04 +0330

    serverless environment config

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,11 +1,11 @@
 "use server";
-import { PrismaClient } from "../../lib/generated/prisma";
+import { prisma } from "@/db/prisma";
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
 
 // Get the latest products
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
+  // const prisma = new PrismaClient();
 
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
@@ -13,11 +13,11 @@ export async function getLatestProducts() {
   });
 
   // Convert Decimal values to numbers
-  const convertedData = data.map(product => ({
-    ...product,
-    price: Number(product.price),
-    rating: Number(product.rating),
-  }));
+  // const convertedData = data.map((product) => ({
+  //   ...product,
+  //   price: Number(product.price),
+  //   rating: Number(product.rating),
+  // }));
 
-  return convertToPlainObject(convertedData);
+  return convertToPlainObject(data);
 }
