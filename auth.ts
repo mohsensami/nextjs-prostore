3b1beac414df9c0de9066827d7e766465ed99eda
mohsensami <mohsensami18727@gmail.com>
2025-08-29 17:38:07 +0330

    protecting path

--- auth.ts
+++ auth.ts
@@ -85,21 +85,21 @@ export const config = {
             data: { name: token.name },
           });
         }
-        if (trigger === 'signIn' || trigger === 'signUp') {
+        if (trigger === "signIn" || trigger === "signUp") {
           const cookiesObject = await cookies();
-          const sessionCartId = cookiesObject.get('sessionCartId')?.value;
-    
+          const sessionCartId = cookiesObject.get("sessionCartId")?.value;
+
           if (sessionCartId) {
             const sessionCart = await prisma.cart.findFirst({
               where: { sessionCartId },
             });
-    
+
             if (sessionCart) {
               // Overwrite any existing user cart
               await prisma.cart.deleteMany({
                 where: { userId: user.id },
               });
-    
+
               // Assign the guest cart to the logged-in user
               await prisma.cart.update({
                 where: { id: sessionCart.id },
@@ -118,6 +118,22 @@ export const config = {
       return token;
     },
     authorized({ request, auth }: any) {
+      // Array of regex patterns of protected paths
+      const protectedPaths = [
+        /\/shipping-address/,
+        /\/payment-method/,
+        /\/place-order/,
+        /\/profile/,
+        /\/user\/(.*)/,
+        /\/order\/(.*)/,
+        /\/admin/,
+      ];
+      // Get pathname from the req URL object
+      const { pathname } = request.nextUrl;
+
+      // Check if user is not authenticated and on a protected path
+      if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
+      
       // Check for cart cookie
       if (!request.cookies.get("sessionCartId")) {
         // Generate cart cookie
