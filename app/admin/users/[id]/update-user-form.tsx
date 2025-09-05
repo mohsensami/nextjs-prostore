65fa1353c119d47884c2d763a1b2b4508ff6e399
mohsensami <mohsensami18727@gmail.com>
2025-09-05 11:05:50 +0330

    Update user action

--- app/admin/users/[id]/update-user-form.tsx
+++ app/admin/users/[id]/update-user-form.tsx
@@ -25,6 +25,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { updateUser } from "@/lib/actions/user.actions";
+import { toast } from "sonner";
 
 const UpdateUserForm = ({
   user,
@@ -39,9 +41,44 @@ const UpdateUserForm = ({
     defaultValues: user,
   });
 
+  // Handle submit
+  const onSubmit = async (values: z.infer<typeof updateUserSchema>) => {
+    try {
+      const res = await updateUser({
+        ...values,
+        id: user.id,
+      });
+
+      if (!res.success)
+        // return toast({
+        //   variant: "destructive",
+        //   description: res.message,
+        // });
+        toast(res.message);
+
+      //   toast({
+      //     description: res.message,
+      //   });
+      toast(res.message);
+
+      form.reset();
+      router.push(`/admin/users`);
+    } catch (error) {
+      //   toast({
+      //     variant: "destructive",
+      //     description: (error as Error).message,
+      //   });
+      toast((error as Error).message);
+    }
+  };
+
   return (
     <Form {...form}>
-      <form className="space-y-4">
+      <form
+        method="post"
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-4"
+      >
         {/* Email */}
         <div>
           <FormField
