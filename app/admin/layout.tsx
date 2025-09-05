8eac864e4bb2aedc3dfce12fb6ccc7c736c3cca3
mohsensami <mohsensami18727@gmail.com>
2025-09-05 11:15:38 +0330

    Admin search form

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,42 +1,36 @@
 import { APP_NAME } from "@/lib/constants";
 import Image from "next/image";
 import Link from "next/link";
-import { Input } from "@/components/ui/input";
 import Menu from "@/components/shared/header/menu";
 import MainNav from "./main-nav";
+import AdminSearch from "@/components/admin/admin-search";
 
-export default async function AdminLayout({
+export default function AdminLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <>
       <div className="flex flex-col">
         <div className="border-b container mx-auto">
-          <div className="flex h-16 items-center px-4">
+          <div className="flex items-center h-16 px-4">
             <Link href="/" className="w-22">
               <Image
                 src="/images/logo.svg"
-                width={48}
                 height={48}
-                alt={`${APP_NAME} logo`}
+                width={48}
+                alt={APP_NAME}
               />
             </Link>
-            {/* MAIN NAV HERE */}
             <MainNav className="mx-6" />
-            <div className="ml-auto flex items-center space-x-4">
-              <div>
-                <Input
-                  type="search"
-                  placeholder="Search..."
-                  className="md:w-[100px] lg:w-[300px]"
-                />
-              </div>
+            <div className="ml-auto items-center flex space-x-4">
+              <AdminSearch />
               <Menu />
             </div>
           </div>
         </div>
+
         <div className="flex-1 space-y-4 p-8 pt-6 container mx-auto">
           {children}
         </div>
